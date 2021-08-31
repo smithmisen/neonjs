b4afa7d4c5d9dc5ebd423f7dd46100f5433a5e11
krishpranav <krisna.pranav@gmail.com>
2021-08-31 10:46:58 +0530

    helper header
    added helper require for the whole project

--- helper.h
+++ helper.h
@@ -1,7 +1,18 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: MIT License
+ */
+
+// includes
 #include <vector>
 #include <string>
 
-namespace helper {
-    void urldecode(char *dst, const char* rsc);
+using namespace std;
+
+namespace helpers {
+    vector<string> split(const string &s, char delim);
+    string generateToken();
+    void urldecode(char *dst, const char *src);
     char* cstrcopy(string str);
 }
