1d4641235ab0a7291ff58cc06d8dbd75c582ad22
krishpranav <krisna.pranav@gmail.com>
2021-08-31 12:02:50 +0530

    settings header

--- settings.h
+++ settings.h
@@ -1,18 +1,25 @@
 /*
- * Copyright (c) 2021, Michael Smith
+ * Copyright (c) 2021, Michael Smith, Krisna Pranav
  *
  * SPDX-License-Identifier: MIT License
  */
 
+#include "json.hpp"
+
 using namespace std;
 
+using json = nlohmann::json;
 extern bool loadResFromDir;
 
 namespace settings {
+
+    json getConfig();
+
     string getFileContent(string filename);
     string getGlobalVars();
     void setGlobalArgs(json args);
     string joinAppPath(string filename);
     string getMode();
+
     void setPort(int port);
 }
